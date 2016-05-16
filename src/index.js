4458bfaa826415293101f0997bdb63a28f94a3e7
Mateusz Wijas <mateusz.wijas+github@gmail.com>
2016-05-15 23:50:57 -0400

    color controls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { compose, setPropTypes, pure } from 'recompose';
 
 import {
   wrapperStyle,
@@ -8,30 +7,27 @@ import {
   textHeadingStyle
 } from './styles';
 
-function AppStoreBadge({ url, textHeading, textAppStore }) {
-  return (
-    <a
-      className='ReactVendor--AppStoreButton'
-      href={url}
-      style={wrapperStyle}
-    >
-      <i style={iconStyle}>A</i>
+export default function AppStoreBadge({
+    url,
+    textHeading,
+    textAppStore,
+    primaryColor = "#ffffff",
+    secondaryColor = "#000000"
+  }) {
+    return (
+      <a
+        className='ReactVendor--AppStoreButton'
+        href={url}
+        style={wrapperStyle(primaryColor, secondaryColor)}
+      >
+        <i style={iconStyle(primaryColor)}>A</i>
 
-      <span style={textHeadingStyle}>
-        {textHeading || 'Available on the'}
-      </span>
-      <span style={textAppStoreStyle}>
-        {textAppStore || 'App Store'}
-      </span>
-    </a>
-  );
+        <span style={textHeadingStyle(primaryColor)}>
+          {textHeading || 'Available on the'}
+        </span>
+        <span style={textAppStoreStyle(primaryColor)}>
+          {textAppStore || 'App Store'}
+        </span>
+      </a>
+    );
 }
-
-export default compose(
-  setPropTypes({
-    downloadLink: React.PropTypes.string.isRequired,
-    textHeading: React.PropTypes.string,
-    textAppStore: React.PropTypes.string
-  }),
-  pure
-)(AppStoreBadge);
--- src/styles.js
+++ src/styles.js
@@ -1,54 +1,57 @@
-const defaults = {
-  primaryColor: '#ffffff',
-  secondaryColor: '#000000'
-};
+function wrapperStyle(primaryColor, secondaryColor) {
+  return {
+    color: primaryColor,
+    backgroundColor: secondaryColor,
+    border: `1px solid ${primaryColor}`,
+    borderRadius: '3px',
+    display: 'inline-block',
+    boxSizing: 'border-box',
+    fontWeight: 'normal',
+    fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+    height: '66px',
+    marigin: '24px 12px 6px 0',
+    padding: '9px 0',
+    textAlign: 'center',
+    textDecoration: 'none',
+    width: '192px'
+  };
+}
 
-const wrapperStyle = {
-  color: defaults.primaryColor,
-  backgroundColor: defaults.secondaryColor,
-  border: `1px solid ${defaults.primaryColor}`,
-  borderRadius: '3px',
-  display: 'inline-block',
-  boxSizing: 'border-box',
-  fontWeight: 'normal',
-  fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif',
-  height: '66px',
-  marigin: '24px 12px 6px 0',
-  padding: '9px 0',
-  textAlign: 'center',
-  textDecoration: 'none',
-  width: '192px'
-};
+function textAppStoreStyle(primaryColor) {
+    return {
+    color: primaryColor,
+    fontSize: '18px',
+    boxSizing: 'border-box',
+    lineHeight: '24px',
+    position: 'relative',
+    top: '-3px',
+    textTransform: 'none'
+  };
+}
 
-const textAppStoreStyle = {
-  color: '#fff',
-  fontSize: '18px',
-  boxSizing: 'border-box',
-  lineHeight: '24px',
-  position: 'relative',
-  top: '-3px',
-  textTransform: 'none'
-};
+function iconStyle(primaryColor) {
+    return {
+    color: primaryColor,
+    display: 'block',
+    boxSizing: 'border-box',
+    float: 'left',
+    fontSize: '40px',
+    lineHeight: 1.3,
+    position: 'relative',
+    left: '15px'
+  };
+}
 
-const iconStyle = {
-  color: defaults.primaryColor,
-  display: 'block',
-  boxSizing: 'border-box',
-  float: 'left',
-  fontSize: '40px',
-  lineHeight: 1.3,
-  position: 'relative',
-  left: '15px'
-};
-
-const textHeadingStyle = {
-  color: defaults.primaryColor,
-  display: 'block',
-  fontSize: '9px',
-  boxSizing: 'border-box',
-  lineHeight: '24px',
-  textTransform: 'uppercase'
-};
+function textHeadingStyle(primaryColor) {
+  return {
+    color: primaryColor,
+    display: 'block',
+    fontSize: '9px',
+    boxSizing: 'border-box',
+    lineHeight: '24px',
+    textTransform: 'uppercase'
+  };
+}
 
 export {
   wrapperStyle,
